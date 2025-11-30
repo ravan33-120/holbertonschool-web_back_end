63ed34ffc3ce834b440c20415757b657a97ad162
ravan33-120 <ravannabiyev466@gmail.com>
2025-11-30 00:07:22 +0000

    all def

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
+"""
+Function that lists all documents in a MongoDB collection
+"""
+
 def list_all(mongo_collection):
-        return list(mongo_collection.find())
+    """
+    Returns a list of all documents in the collection
+    Returns an empty list if the collection is empty
+    """
+    return list(mongo_collection.find())
