62abd2f3c31bb9f5f44d590bd7c215030c31b4b7
ravan33-120 <ravannabiyev466@gmail.com>
2025-11-30 03:13:34 +0000

    logs

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -9,20 +9,12 @@ if __name__ == "__main__":
     client = MongoClient("mongodb://127.0.0.1:27017")
     collection = client.logs.nginx
 
-    # 1. Ümumi log sayı
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
+    print(f"{collection.count_documents({})} logs")
 
-    # 2. Metod statistikasını çap edirik
     print("Methods:")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
 
     for m in methods:
-        count = collection.count_documents({"method": m})
-        print(f"\tmethod {m}: {count}")
+        print(f"\tmethod {m}: {collection.count_documents({'method': m})}")
 
-    # 3. GET + /status sorğuları
-    status_check = collection.count_documents(
-        {"method": "GET", "path": "/status"}
-    )
-    print(f"{status_check} status check")
+    print(f"{collection.count_documents({'method': 'GET', 'path': '/status'})} status check")
